fb225187f95d9822df56420a54be13782ae0f02c
Ilya Denisov <i.denisovs@gmail.com>
2020-11-07 17:20:44 +0200

    Different approach defining the point.

--- source/geometry/Point.ts
+++ source/geometry/Point.ts
@@ -1,13 +1,1 @@
-export default class Point {
-    x: number;
-    y: number;
-
-    constructor(x = 0, y = 0) {
-        this.x = x;
-        this.y = y;
-    }
-
-    toString() {
-        return `{ ${this.x}, ${this.y} }`;
-    }
-}
+export type Point = [ x: number, y: number ];
--- source/geometry/index.ts
+++ source/geometry/index.ts
@@ -1,8 +1,11 @@
-import Point from './Point';
+import { Point } from './Point';
 
 export function distance(p1: Point, p2: Point): number {
-    const dX2 = Math.pow(p2.x - p1.x, 2);
-    const dY2 = Math.pow(p2.y - p1.y, 2);
+    const [ x1, y1 ] = p1;
+    const [ x2, y2 ] = p1;
+
+    const dX2 = Math.pow(x2 - x1, 2);
+    const dY2 = Math.pow(y2 - y1, 2);
 
     return Math.sqrt(dX2 + dY2);
 }
