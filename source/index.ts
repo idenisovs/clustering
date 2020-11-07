38be77e15ef85aa5cdb8f54c4e7b9d599d3ffd2a
Ilya Denisov <i.denisovs@gmail.com>
2020-11-07 16:10:47 +0200

    Canvas takes full width.

--- source/index.ts
+++ source/index.ts
@@ -2,6 +2,9 @@ window.onload = function init() {
     const canvas = document.getElementById('canvas') as HTMLCanvasElement;
     const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
-    ctx.fillStyle = 'green';
-    ctx.fillRect(200, 10, 100, 100);
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+    ctx.fillStyle = 'navy';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 };
