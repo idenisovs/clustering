b5d39625b9e0799900cf07e39e5ecc8413153550
Ilya Denisov <i.denisovs@gmail.com>
2020-11-07 18:32:01 +0200

    Distance measuring function is fixed.

--- source/geometry/index.ts
+++ source/geometry/index.ts
@@ -2,7 +2,7 @@ import { Point } from './Point';
 
 export function distance(p1: Point, p2: Point): number {
     const [ x1, y1 ] = p1;
-    const [ x2, y2 ] = p1;
+    const [ x2, y2 ] = p2;
 
     const dX2 = Math.pow(x2 - x1, 2);
     const dY2 = Math.pow(y2 - y1, 2);
